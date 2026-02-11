2accebada7d806a8661cb70d040209878565d3de
SomyaBaran <somyabaranwal6664@gmail.com>
2026-02-12 05:02:18 +0530

    init

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -7,7 +7,23 @@ export function Career() {
             marginTop: "16px",
             backgroundColor: "white",
         }}>
+            <p style={{
+                fontSize: "0.8rem",
+                marginLeft: "10px",
+                marginTop: "6px",
+                color: "rgb(128, 128, 128)",
+            }}>
+                Achieve your career goals
+            </p>
 
+            <div style={{
+                height: "15px",
+                width: "15px",
+                background: "linear-gradient(135deg, #E7A33E 50%, #C4812B 50%)",
+                borderRadius: "2px",
+                marginLeft: "10px"
+            }}>
+            </div>
         </div>
     )
 }
