5386827a14a3f8cafcb0a450d755608fb563e00c
SomyaBaran <somyabaranwal6664@gmail.com>
2026-02-14 23:38:32 +0530

    init

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,4 @@
-export function Content(){
+export function Content() {
     return (
         <div style={{
             height: "65vh",
@@ -7,7 +7,36 @@ export function Content(){
             borderRadius: "20px",
             border: "1px solid rgb(190, 189, 189)",
             backgroundColor: "white"
-        }}>  
+        }}>
+            <div style={{
+                height: "6.8vh",
+                width: "4vw",
+                border: "1px solid rgb(190, 189, 189)",
+                marginLeft: "20px",
+                marginTop: "20px",
+                borderRadius: "50%",
+                overflow: "hidden"
+            }}>
+                <img
+                    src="https://cdn.vectorstock.com/i/1000v/58/32/cute-cat-kawaii-chibi-drawing-style-vector-45305832.jpg"
+                    alt="Cute cat"
+                    style={{
+                        marginBottom: "-6px",
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover"
+                    }}
+                />
+            </div>
+            <h4 style={{
+                fontSize: "17.5px",
+                marginLeft: "6vw",
+                marginTop: "-50px",
+                color: "rgb(53, 53, 53)",
+                fontWeight: "500"
+            }}>
+                Somya Baranwal
+            </h4>
         </div>
     );
 }
